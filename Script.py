0e84279e2574b9169172886635273c95a04e71b1
DenEdwards <denzelsedwards@gmail.com>
2020-05-06 22:58:15 -0400

    First commit

---
+++ Script.py
@@ -0,0 +1,39 @@
+import tweepy
+import random
+from credentials import consumer_secret, consumer_key, access_token, access_token_secret
+
+# def create_random_story():
+#     bodyPart = random.choice(["head", "stomach", "ear", "face", "eye", "nose", "heart", "cheek", "arm", "mouth", "tooth", "leg", "foot", "toe"])
+#     adj = random.choice(["blackens and curls", "crumbles", "grows huge", "crawls", "is eaten", "melts", "falls off", "turns into nothing", "shines in the dark", "disappears", "turns black"])
+#     person = random.choice(["father", "mother", "sister", "brother", "teacher", "co-worker", "manager", "son", "daughter"])
+#     action = random.choice(["die", "fly away", "leave", "smile", "frown", "laugh", "melt", "disappear", "chuckle"])
+#     place = random.choice(["with a rose", "with a sunflower", "with an old friend", "with an old enemy", "with your journal", "with your diary", "with oil paints", "with a mango", "with yourself", "with another", "in the searing desert", "in your home", "in the mall", "in the park", "outside", "inside", "in your parent's bedroom", "in the southeast corner of your old school"])
+#     des = random.choice(["favorite", "neglected", "beloved", "huge", "small"])
+#     final = random.choice(["forever", "without a word", "silently", "quickly", "spontaneously", "over and over again", "badly"])
+#
+#     intro1 = "Your " + bodyPart + " " + adj + ", as you watch your " + person + " " + action + " " + final + "."
+#     intro2 = "Alone " + place + ", your " + des + " " + bodyPart + " " + adj + "."
+#     intro3 = "You didn't expect your " + person + " to point out your " + des + " " + bodyPart + "."
+#
+#     intros = [intro1, intro2, intro3]
+#     story = random.choice(intros);
+#
+#     print(story)
+#
+#
+# create_random_story()
+#
+# Authenticate to Twitter
+# telling Tweepy to use the credentials that created
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# creates an API object that you can use to invoke Twitter API methods. Setting wait_on_rate_limit and wait_on_
+# rate_limit_notify to True makes the API object print a message and wait if the rate limit is exceeded
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+except:
+    print("Error during authentication")
